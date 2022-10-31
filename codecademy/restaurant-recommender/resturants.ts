765796d085226473ad965281297eb1d9f7f31d51
Alice Lee <alicejeeuncodes@gmail.com>
2022-10-31 18:17:46 +0000

    Solved Restaurant Recommender challenge.

---
+++ codecademy/restaurant-recommender/resturants.ts
@@ -0,0 +1,44 @@
+const restaurants = [
+  {
+    name: 'Silver Rice Sushi 🍣',
+    priceBracket: '2',
+    deliveryTimeMinutes: 40,
+    openHour: '12',
+    closeHour: '23',
+    distance: '5',
+  },
+  {
+    name: 'Nikko\'s Rotisserie Chicken 🍗',
+    priceBracket: '1',
+    deliveryTimeMinutes: 20,
+    openHour: '12',
+    closeHour: '21',
+    distance: '8',
+  },
+  {
+    name: 'Aita Trattoria 🍝',
+    priceBracket: '3',
+    deliveryTimeMinutes: 60,
+    openHour: '18',
+    closeHour: '22',
+    distance: '1',
+  },
+  {
+    name: 'Lula Bagel 🥯',
+    priceBracket: '1',
+    deliveryTimeMinutes: 20,
+    openHour: '0',
+    closeHour: '12',
+    distance: '2',
+  },
+  {
+    name: 'Golden Chopstick 🥢',
+    priceBracket: '1',
+    deliveryTimeMinutes: 20,
+    openHour: '15',
+    closeHour: '23',
+    distance: '12',
+  },
+];
+
+export default restaurants;
